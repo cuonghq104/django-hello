8b46d225d3af64011a22f32158369aa84f8dbe17
CuongHQ <cuonghq@smartsi.com>
2025-08-04 00:32:37 +0700

    Init commit

---
+++ api/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from api.models import Product, Order, OrderItem
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock'
+        )
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0"
+            )
+        return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product',
+            'quantity',
+            'product_name',
+            'product_price',
+            'item_subtotal'
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.order_items.all()
+        return sum(item.item_subtotal for item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'order_items',
+            'total_price'
+        )
