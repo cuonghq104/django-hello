82945ea87e4bf3b57595eb245e81d207294a3011
cuonghq <cuonghq104@gmail.com>
2025-08-07 18:56:00 +0700

    Add filter & search features

--- api/serializers/order_serializers.py
+++ api/serializers/order_serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from api.models import Order, OrderItem
@@ -16,6 +17,53 @@ class OrderItemSerializer(serializers.ModelSerializer):
             'product_price',
             'item_subtotal'
         )
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    class OrderItemCreateSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = OrderItem
+            fields = (
+                'product',
+                'quantity',
+            )
+
+    order_id = serializers.UUIDField(read_only=True)
+    order_items = OrderItemCreateSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'order_items'
+        )
+        extra_kwargs = {
+            'user': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        order_items = validated_data.pop('order_items')
+        with transaction.atomic():
+            order = Order.objects.create(**validated_data)
+
+            for item in order_items:
+                OrderItem.objects.create(order=order, **item)
+
+        return order
+
+    def update(self, instance, validated_data):
+        order_items = validated_data.pop('order_items')
+        with transaction.atomic():
+            instance = super().update(instance, validated_data)
+
+            if order_items is not None:
+                instance.order_items.all().delete()
+
+                for item in order_items:
+                    OrderItem.objects.create(order=instance, **item)
+
+            return instance
 
 
 class OrderSerializer(serializers.ModelSerializer):
--- api/views/order_views.py
+++ api/views/order_views.py
@@ -1,10 +1,14 @@
+from xmlrpc.client import Fault
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from api.filters.order_filters import OrderFilter
 from api.models import Order
-from api.serializers import OrderSerializer
+from api.serializers import OrderSerializer, OrderCreateSerializer
 
 
 # class OrderListApiView(generics.ListAPIView):
@@ -27,3 +31,17 @@ class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     filterset_class = OrderFilter
     filter_backends = [DjangoFilterBackend]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST' or self.action == 'update':
+            return OrderCreateSerializer
+        return super().get_serializer_class()
+
+    def perform_create(self, serializer):
+        serializer.save(status=Order.StatusChoice.PENDING, user=self.request.user)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='user-orders')
+    def get_user_orders(self, request, pk=None):
+        qs = self.get_queryset().filter(user=self.request.user)
+        data = self.get_serializer(qs, many=True)
+        return Response(data.data)
